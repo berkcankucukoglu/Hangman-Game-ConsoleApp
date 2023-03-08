27385b21766ad0f9defd5a9e794eec22fd424bec
Berkcan <berkcankucukoglu@gmail.com>
2023-03-08 17:14:05 +0300

    Add project files.

---
+++ HangmanConsoleApp/Program.cs
@@ -0,0 +1,59 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var selectedWord = "test";
+        var controlString = string.Empty;
+        var lives = 3;
+        var timesPlayed = 0;
+        Console.WriteLine($"Please guess the word that has {selectedWord.Length} characters!");
+        for (int i = 0; i < selectedWord.Length; i++)
+        {
+            controlString += '*';
+            Console.Write('*');
+        }
+        var liveTestString = controlString;
+        Console.WriteLine();
+        Console.WriteLine($"You have {lives} guesses. Please make a guess: \n");
+        while (lives > 0)
+        {
+            var key = Console.ReadKey(true);
+
+            for (int j = 0; j < selectedWord.Length; j++)
+            {
+                if (selectedWord[j] == key.KeyChar)
+                {
+                    controlString = controlString.Remove(j, 1);
+                    controlString = string.Concat(controlString.Substring(0, j), key.KeyChar, controlString.Substring(j));
+                }
+            }
+            if (controlString == liveTestString && !controlString.Contains(key.KeyChar))
+            {
+                lives--;
+                liveTestString = controlString;
+                Console.WriteLine($"You made a wrong guess! The letter \"{key.KeyChar}\" is not found in the word!");
+                if (lives != 0)
+                {
+                    Console.Write($"Now you have {lives} tries. Please make another guess:\n");
+                }
+            }
+            if (controlString != liveTestString)
+            {
+                liveTestString = controlString;
+                Console.WriteLine($"Bravo! \"{key.KeyChar}\" is in the word!");
+            }
+            timesPlayed++;
+            Console.WriteLine(controlString);
+            if (!controlString.Contains('*'))
+            {
+                Console.WriteLine($"You made {timesPlayed} guesses in total!");
+                Console.WriteLine("Victory! You found the right word!");
+                break;
+            }
+        }
+        if (lives == 0)
+        {
+            Console.WriteLine($"GGs! the right word was:\n{selectedWord}");
+        }
+    }
+}
